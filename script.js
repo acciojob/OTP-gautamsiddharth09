6445b65d6c6d026c61c8f1acad13fd57cabd293e
Gautam kumar <gautamsiddharth2013@gmail.com>
2025-10-16 13:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let inputs =  document.querySelectorAll(".code");
-//inputs[0].focus();
+inputs[0].focus();
 
 inputs.forEach((input,index) =>{
 	input.addEventListener("input", (e) =>{
